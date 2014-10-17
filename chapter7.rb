8952c1f3bb47cc5c0431acf0e91d2425a75ceced
Anthony Albertorio <anthony.albertorio@gmail.com>
2014-10-17 14:20:00 -0400

    added 99 beers on a wall problem to chapter 7 notes, chapter7.rb

--- chapter7.rb
+++ chapter7.rb
@@ -70,4 +70,249 @@ else
 end
 
 
+#another sub program example
+#harsh 7th grade teacher lol
+#its important to keep indentations,
+#if not program will be hard to read
+#comments are very useful in programming
+#to help maintain code
+#other people might work on code too
+#so its good for them to know whats going on.
 
+#writing code and testing it out as you do it 
+#is a great way to break the problem down 
+#into modular bits
+#the author stated that putting in the
+#if else end first, then concentrating
+#on portions that he needed to work on 
+#simplifies the process, and allows for 
+#avoid erros and testing, every step of the
+#way
+#a great point.
+
+puts 'Hello, and welcome to seventh grade English'
+puts 'My name is Mrs. Gabbard. And your name is...?'
+name = gets.chomp
+
+if name == name.capitalize
+	puts 'Please take a seat, ' + name '.'
+else 
+	puts name + '? you mean ' + name.capitalize + ', right?'
+	puts 'Don\'t you even know how to spell your name??'
+	reply = gets.chomp
+
+	if reply.downcase == 'yes'
+		puts 'Hmmph! Well, sit down!'
+	else
+		puts 'GET OUT!'
+	end
+end
+
+#7.3 Looping
+#in looping make sure to explictly state what conditions 
+#should end the loop or you will have an infinite loop
+#if condition is always true, then the loop will
+#run forever, until something else prompts it to be false
+
+#while statement
+#while tests your condition before looping.
+
+#though this program works, it is a bit awkward  
+input =  ''
+
+while input != 'bye'
+	puts input
+	input = gets.chomp
+end
+
+puts 'Come again soon!'
+
+#always true program
+#this is a more elegent (and simple) program than the one on top
+
+while true
+	input = gets.chomp
+	puts input
+	if input == 'bye'
+		break			#break breaks out of loop
+	end
+end
+
+puts 'Come again soon'
+
+#7.4 A little bit of logic
+
+#In this sub-program
+#returns what a lovely name if either Chris or Katy is mentioned
+#exits if it is not.
+
+
+#this is an ok program
+#not amazing because the way it looks. 
+#Chris and Katy are equivalent, but Katy
+#looks subordinate to Chris 
+puts 'Hello, what\'s your name?'
+name = gets.chomp
+puts 'Hello, ' + name + '.'
+
+if name == 'Chris'
+	puts 'What a lovely name!'
+else 
+	if name == 'Katy'
+		puts 'What a lovely name!'
+	end
+end
+
+#this is a better program
+#elsif solves that
+#note that itis elsif not elseif
+#the e is missing.
+#but still repeats itself
+
+puts 'Hello, what\'s your name?'
+name = gets.chomp
+puts 'Hello, ' + name + '.'
+
+if name == 'Chris'
+	puts 'What a lovely name!'
+elsif name == 'Katy'
+	puts 'What a lovely name!'
+end
+
+#logical operators
+#best program
+#uses logical operator || or (inclusive)
+#meaning it can be true if one is true
+#or if both is true
+#meaning one or the other or both 
+
+#In english we use the exlcusive or XOR implicitly
+#do you want soup or salad (but not both)".
+#aka one or the other, but not both
+#|| means or
+#it reduces the complexity and keeps with DRY principle
+
+puts 'Hello, what\'s your name?'
+name = gets.chomp
+puts 'Hello, ' + name + '.'
+
+if name == 'Chris' || name == 'Katy'
+	puts 'What a lovely name!'
+end
+
+#&& is and
+#! is not
+#works with fundamentals of logic tables in discrete structures
+
+i_am_chris = true
+i_am_purple = false
+i_like_beer = true
+i_eat_rocks = false
+
+puts i_am_chris && i_like_beer			#true
+puts i_like_beer && i_eat_rocks			#false
+puts i_am_chris && i_like_beer			#false			
+i_am_purple && i_eat_rocks				#false
+
+puts #space
+
+puts i_am_chris || i_like_beer			#true
+puts i_like_beer || i_eat_rocks			#true
+i_am_purple || i_like_beer				#true
+i_am_purple || i_eat_rocks				#false
+
+puts !i_am_purple						#true
+puts !i_am_chris						#false
+
+
+#you can use 'true' boolean value 
+#but it still says stop before we can exit out the program
+#aka the loop runs once before we can stop
+while true 
+	puts 'What would you like to ask C to do?'
+	request = gets.chomp
+
+	puts 'You say, "C, please ' + request + '"'
+	puts '"C ' + request + '."'
+	puts '"Papa ' + request + '."'
+	puts '"Mama ' + request + '."'
+	puts '"Ruby ' + request + '."'
+	puts '"Nono ' + request + '."'
+	puts '"Emma ' + request + '."'
+
+	if request == 'stop'
+		break
+	end
+end
+
+#7.4 A Few Things to Try
+
+#“99 Bottles of Beer on the Wall.” 
+#Write a program that prints out the lyrics 
+#to that beloved classic, “99 Bottles of Beer on the Wall.”
+puts 'Lets sing a song!'
+counter = 99
+while counter <= 99 && counter != 0
+    puts counter.to_s + ' bottles of beer on the wall, ' + counter.to_s + ' bottles of beer.'
+    puts 'Take one down, pass it around, ' + counter.to_s + ' bottles of beer on the wall...'
+    
+    counter = counter - 1
+
+    if counter == 0
+		puts counter.to_s + ' bottles of beer, on the wall!'
+		puts 'Hey, lets go to the store and get some more!'
+		break
+	end
+end
+
+
+
+
+#Deaf grandma. 
+#Whatever you say to Grandma (whatever you type in), 
+#she should respond with this:
+#HUH?! SPEAK UP, SONNY!
+#unless you shout it (type in all capitals). 
+#If you shout, she can hear you (or at least she thinks so) 
+#and yells back:
+#NO, NOT SINCE 1938!
+
+#To make your program really believable, 
+#have Grandma shout a different year each time, 
+#maybe any year at random between 1930 and 1950. 
+#(This part is optional and would be much easier 
+#if you read the section on Ruby’s random number generator 
+#on page 38.) You can’t stop talking to Grandma 
+#until you shout BYE.
+#Hint 1: Don’t forget about chomp! 'BYE' with an Enter 
+#at the end is not the same as 'BYE' without one!
+#Hint 2: Try to think about what parts of your program 
+#should happen over and over again. 
+#All of those should be in your while loop.
+#Hint 3: People often ask me, 
+#“How can I make rand give me a number in a range not 
+#starting at zero?” Well, you can’t; rand just doesn’t work 
+#that way. So, I guess you’ll have to do something to 
+#the number rand returns to you.
+
+
+
+#Deaf grandma extended. 
+#What if Grandma doesn’t want you to leave?
+#When you shout BYE, she could pretend not to hear you. 
+#Change your previous program so that you have to shout BYE 
+#three times in a row. 
+#Make sure to test your program: 
+#if you shout BYE three times but not in a row, 
+#you should still be talking to Grandma.
+
+
+#Leap years. 
+#Write a program that asks for a starting year 
+#and an ending year and then puts all the leap years 
+#between them (and including them, if they are also leap years). 
+#Leap years are years divisible by 4 (like 1984 and 2004). 
+#However, years divisible by 100 are not leap years 
+#(such as 1800 and 1900) unless they are also divisible by 400 
+#(such as 1600 and 2000, which were in fact leap years). 
+#What a mess!
